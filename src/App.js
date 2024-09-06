0a4df4a7b3554a4d4851e420f99a656af04be797
Manupriyan <manupriyan722@gmail.com>
2024-09-06 21:28:12 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./pages/global/TopBar";
+import { ColorModeContext, useMode } from "./theme";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/index";
+
 const App = () => {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
